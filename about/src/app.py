9bef3b8b34067ab8494e9a19194afa0cbc6c4d3e
Kyle Kernick <kkernick@ualberta.ca>
2024-01-17 16:05:52 -0700

    Tweak

--- about/src/app.py
+++ about/src/app.py
@@ -11,7 +11,6 @@ from gallery import gallery
 introduction = """
 <!DOCTYPE html>
 <html>
-  <head>
 		<meta charset="UTF-8">
 		
 		<title>Contact</title>
@@ -22,36 +21,10 @@ introduction = """
 		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 		
 		<script>
-			$(function(){ 
-				$("#navbar").load("../../../www/navbar.html", function(){ 
-					$('#aboutTab').addClass('active');
-					$('#heatmapper-logo').on('click', function(){ window.location.href = '/'; });
-				});
-			}); 
+			$(function(){$("#introduction").load("/../../input/introduction.html"}); 
 		</script>
 		
 		<script type="text/javascript" src="../../../www/js/google-analytics.js"></script>
-		
-  </head>  
-	
-  <body>
-		<div id="navbar"></div>
-		<div class="container-fluid well">
-			<h2>Contact</h2>
-			Questions or comments? Please use our <a href="http://feedback.wishartlab.com/?site=heatmapper" target="_blank">feedback page</a>.
-			<br />
-			<br />
-			This project was developed in the <a href="http://www.wishartlab.com/" target="_blank">Wishart Research Group</a> at the University of Alberta.<br />
-			Financial support was provided by the <a href="http://www.cihr-irsc.gc.ca" target="_blank">Canadian Institutes of Health Research (CIHR)</a><br />
-			and by <a href="http://genomealberta.ca" target="_blank">Genome Alberta</a>, a division of Genome Canada.
-			<br />
-			Information on file format requirements can be found on the <a href="/about/instructions">Instructions Page</a>. 
-			<br />
-			All source code for this project can be found on <a href="https://github.com/sbabicki/heatmapper" target="_blank">GitHub</a>.
-			<br />
-			License: <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">Creative Commons Attribution-ShareAlike 2.0 Generic</a>
-		</div>
-	</body>
 </html>
 """
 
