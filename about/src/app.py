d8700d18484a5fe34fc969340a80788eed62a1ad
Kyle Kernick <kkernick@ualberta.ca>
2024-01-17 16:00:41 -0700

    fix

--- about/src/app.py
+++ about/src/app.py
@@ -81,8 +81,6 @@ def server(input: Inputs, output: Outputs, session: Session):
 
 app_ui = ui.page_fluid(
 	ui.panel_title(ui.HTML('<a href="https://kkernick.github.io">Heatmapper</a>')),
-
-	ui.HTML(load),
 	ui.layout_sidebar(
 		ui.sidebar(
 
