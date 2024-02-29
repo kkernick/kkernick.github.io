cd5294ef7e9444e02c9a0dfb44da340567a3ec39
Kyle Kernick <kkernick@ualberta.ca>
2024-02-29 00:33:17 -0700

    Revert ""Symlink""
    This reverts commit 47466804a9aa0560d557d14809038b10dbd3475f.

--- symlink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Unsurprisingly, ShinyLive doesn't support symlinks. Therefore, just copy them
-for project in "expression" "geocoordinate" "geomap" "image" "pairwise"; do
-	cp "./shared/shared.py" "./${project}/src/shared.py"
-done
