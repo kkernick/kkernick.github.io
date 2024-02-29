c79b0eb02cba47710962ca4c2abd705cb271ba9a
Kyle Kernick <kkernick@ualberta.ca>
2024-02-29 11:53:18 -0700

    Document shared.py

--- shared/shared.py
+++ shared/shared.py
@@ -1,3 +1,11 @@
+#
+# Heatmapper
+# Shared
+#
+# This file contains shared functionality between Heatmapper applications. It is not a standalone application.
+# Due to the way ShinyLive exports applications, this file is symlinked into each project to reduce redundancy.
+#
+
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 from shiny.types import FileInfo
 from pandas import DataFrame, read_csv, read_excel, read_table
@@ -6,39 +14,67 @@ from sys import modules
 from copy import deepcopy
 from pathlib import Path
 
+# If pyodide is found, we're running WebAssembly.
 if "pyodide" in modules:
 	from pyodide.http import pyfetch
 	Pyodide = True
+# Otherwise,
 else:
 	from os.path import exists
 	Pyodide = False
 
 
 class Cache:
+	"""
+	@brief A class that encompasses fetching/storing web resources.
+	"""
 
 	@staticmethod
 	def DefaultHandler(n, i):
+		"""
+		@brief The default handler. It can handle csv, xlsx, and defaults all other files to read_table
+		@param n: The name of the file. We use this for pattern matching against the suffix.
+		@param i: The binary of the file (Either via read() or BytesIO())
+		@returns: A null-filled DataFrame.
+		"""
 		match Path(n).suffix:
 			case ".csv": df = read_csv(i)
 			case ".xlsx": df = read_excel(i)
 			case _: df = read_table(i)
 		return df.fillna(0)
 
+
 	@staticmethod
 	async def Remote(url): r = await pyfetch(url); return await r.bytes() if r.ok else None
+
 
 	@staticmethod
 	async def Local(url): return open(url, "rb").read() if exists(url) else None
 
 
 	def __init__(self, project, DataHandler = DefaultHandler):
+		"""
+		@brief Initialize an instance of the Cache object.
+		@param project: The name of the project. This is used to fetch web resources.
+		@param DataHandler:	The function that should be called to process files. It should
+												take a name, and a binary stream, and return a DataFrame.
+		"""
+
+		# The primary cache is immutable, and is used when the resource has not been fetched before.
 		self._primary = {}
+
+		# The secondary cache is mutable, and is populated by the primary cache. Purge deletes from here.
 		self._secondary = {}
+
+		# The data handler for processing the binary files.
 		self._handler = DataHandler
 
+		# If we're in a Pyodide environment, we fetch resources from the web.
 		if Pyodide:
 			self.Download = lambda url: Cache.Remote(url)
 			self.Source = "https://raw.githubusercontent.com/kkernick/kkernick.github.io/main/{}/example_input/".format(project)
+
+		# Otherwise, we fetch locally.
 		else:
 			self.Download = lambda url: Cache.Local(url)
 			self.Source = "../example_input/"
@@ -48,6 +84,15 @@ class Cache:
 
 
 	async def N(self, input):
+		"""
+		@brief Caches whatever the user has currently uploaded/selection, returning the identifier within the secondary cache.
+		@param input: The Shiny input variable. Importantly, these must be defined:
+			input.File: The uploaded file
+			input.Example: The selected example file
+			input.SourceFile: Whether the user wants "Upload" or "Example"
+		@returns: The identifier. You should probably use Load() unless you need this.
+		"""
+
 		# Grab an uploaded file, if its done, or grab an example (Using a cache to prevent redownload)
 		if input.SourceFile() == "Upload":
 			file: list[FileInfo] | None = input.File()
@@ -68,9 +113,17 @@ class Cache:
 
 
 	async def Update(self, input):
+		"""
+		@brief Updates information within the secondary cache based on user selection
+		@param input: The Shiny input. Importantly, these must be defined:
+			input.TableRow: The row to modify
+			input.TableCol: The column to modify
+			input.TableVal: What the user wants to set as the new value
+		@info This function should be called on a reactive hook for a "Update" button.
+		"""
+
 		# Get the data
 		df = await self.Load(input)
-
 		row_count, column_count = df.shape
 		row, column = input.TableRow(), input.TableCol()
 
@@ -83,6 +136,11 @@ class Cache:
 
 
 	async def Purge(self, input):
+		"""
+		@brief Purges the secondary cache of whatever the user has uploaded/selected
+		@param input: The Shiny input. See N() for required objects.
+		@info This function should be called on a reactive hook for a "Reset" button.
+		"""
 		if input.SourceFile() == "Upload":
 			file: list[FileInfo] | None = input.File()
 			if file is None: return None
@@ -93,6 +151,11 @@ class Cache:
 
 
 def NavBar(current):
+	"""
+	@brief Returns a Navigation Bar for each project, with the current project selected.
+	@returns A list, containing a ui.panel_title, and a ui.navset_bar.
+	"""
+
 	return [
 			ui.panel_title(title=None, window_title="Heatmapper"),
 
@@ -110,6 +173,17 @@ def NavBar(current):
 
 
 def FileSelection(examples, types):
+	"""
+	@brief Returns the file selection dialog for the user to upload/select an example
+	@param examples: Either a list of example file names, or a dictionary mapping
+	@param types: The valid file extensions for user uploaded files.
+	@returns A list, containing the necessary ui components for uploading/selecting
+	@info The returns elements are named:
+		input.SourceFile: The ui.input_radio_buttons for whether the user wants to choose an "Example" or "Upload"
+		input.File: The ui.input_file for user uploaded files.
+		input.Example: The ui.input_select for an example file selection
+	"""
+
 	# If the user needs help with the formatting.
 	return [ui.HTML('<a href=https://kkernick.github.io/about/site/index.html>Data Format</a>'),
 
@@ -133,6 +207,7 @@ def FileSelection(examples, types):
 	)]
 
 
+# The Table element
 Table = ui.nav_panel("Table",
 	ui.layout_columns(
 		ui.input_numeric("TableRow", "Row", 0),
